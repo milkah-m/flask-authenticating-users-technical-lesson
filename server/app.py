b9fd486695e2e73d9a9d953afb2caed735e421da
Milkah Mwangi <gathonimilkahh@gmail.com>
2026-04-13 15:23:04 +0300

    I learned some basic authentication code using flask and the Resource package from flask_restful

--- server/app.py
+++ server/app.py
@@ -18,5 +18,38 @@ api = Api(app)
 
 # Define routes
 
+class Login(Resource):
+
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+        
+        if user:
+            session['user_id'] = user.id
+            return UserSchema().dump(user)
+        else:
+            return {'message': 'Invalid login'}, 401
+api.add_resource(Login, '/login')
+
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return UserSchema().dump(user)
+        else:
+            return {'message': '401: Not Authorized'}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+api.add_resource(Logout, '/logout')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
